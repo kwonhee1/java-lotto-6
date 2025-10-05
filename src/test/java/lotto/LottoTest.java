f4b750e315fe6acacd3f7f30b98d3fcfa9bbc6fc
kwonhee1 <lkh8033@gmail.com>
2025-10-05 15:33:45 +0900

    LottoTest

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
-import lotto.model.Lotto;
+import lotto.model.lotto.Lotto;
+import lotto.model.machine.LottoResultType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
